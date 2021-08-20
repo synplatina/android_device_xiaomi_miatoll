98d7baa791c5d1877467201713d50c00ed2f701d
synplatina <rizalnoerdin13@gmail.com>
2021-08-20 02:02:22 +0000

    miatoll: PixelPlus

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -7,14 +7,19 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common PixelPlus stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GAPPS=true
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_FACE_UNLOCK_SUPPORTED = true
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := aosp_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
@@ -23,4 +28,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Maintainer
-DEVICE_MAINTAINER := SonalSingh18
+CUSTOM_BUILD_TYPE=Relaxa
+ro.ppui.maintainer_name=relaxaqt
